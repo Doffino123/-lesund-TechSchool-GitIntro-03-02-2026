629f4fbd8c18b938ca914ecb7b02b820884dd8dd
Doffino <danjoh27@gmail.com>
2026-02-03 19:10:42 +0100

    Updated index.ts

--- index.ts
+++ index.ts
@@ -1,3 +1,9 @@
+/**
+ * function that greets a person
+ * @param firstname First name
+ * @param lastname Last name
+ */
+
 export const greeting = (name: string) => {
   console.log(`Hello ${name}`);
 };
